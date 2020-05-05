4d73f7795fd2a81dbfa67f340d30a953845107c9
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-05-05 20:38:03 +0530

    modified test for the add

--- test/test.c
+++ test/test.c
@@ -16,9 +16,13 @@ void show_result(Status result, char *description)
 
 void test_add_to_end(List_ptr list)
 {
-  char *description = "should add element in the end of the list";
+  char *test1 = "Should add element when list is empty";
   Status result = add_to_end(list, 8);
-  show_result(result, description);
+  show_result(result, test1);
+
+  char *test2 = "Should add element when list has when list has elements";
+  result = add_to_end(list, 10);
+  show_result(result, test2);
 }
 
 int main(void)
